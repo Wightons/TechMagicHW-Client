3e1265528424364ea1cc2fb719045795ed9cf6e5
Pavel <kolomiietspavlo881@gmail.com>
2025-02-05 12:26:17 +0200

    v4: generate components

---
+++ client/src/app/components/work-types/work-types.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-work-types',
+  standalone: true,
+  imports: [],
+  templateUrl: './work-types.component.html',
+  styleUrl: './work-types.component.css'
+})
+export class WorkTypesComponent {
+
+}
